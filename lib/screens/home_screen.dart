20ed26b028c2f29bd78a721c4ddc6da0d4efde4b
Paul-202425 <p.masamvu@alustudent.com>
2026-06-11 01:16:03 +0200

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -85,7 +85,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
               );
-            }).toList(),
+            }),
           ],
         ),
       ),
